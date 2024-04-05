29c1b86c9a4882dabed9b5947d448663d8cb2a93
felixmaldonadoos <famaldonadoos@gmail.com>
2024-04-05 18:10:33 -0500

    init source

---
+++ README.md
@@ -0,0 +1,107 @@
+# Adverserial Dynamics in Virtual Reality 
+
+
+### Project Overview
+[**Control a Mouse (Prey) in VR**]
+This Virtual-Reality game maps your movement to a real robot in pursuit of a mouse. The purpose of this project is to elicit planning behaviors from mice as they evade a hostile predator in the form of a robot
+
+[**Planning Perspective**]
+The purpose of these experiments are to understand planning within mammals. We elicit this behavior in mice by having the mice evade a predator in the form of a robot that emits compressed air when the mice gets too close. This experimental paradigm is based on the ```paper link``` from the ```MacIver Lab at Northwestern University ```
+ 
+One of the problems with the current experiment is that the autonomous robot is purely reactive. When we have a mouse that can outrun the robot, the robot itself needs to start planning in order to better simulate a predator. This is the purpose behind my project, to use a real-time planning agent to control the robot. And what better planning agent than a human!
+
+
+[**The Task**]
+Originally, we have an autonomous robot predator that chases after a mouse. It will move towards non-visible locations until it sees the mouse. After it spots the mouse, it will begin chasing after it. At any point, if it loses sight of the mouse, it will go to the last known location.
+<!-- ![Task Diagram](images/task_logic_sq.png) -->
+
+
+## Robotics Control
+<div style="text-align: center;" markdown="1">
+   <img src="images/robot_ctrl_2x.gif" alt="2x Speed of Robot Moving"/>
+</div>
+&nbsp
+
+<div style="text-align: center;" markdown="1">
+   <img src="images/robot_sq.png" alt="Breakdown of the Robot"/>
+   <figcaption> The Skid Steer Drive Robot, designed by Gabrielle Wink and Dr. Angel Germán Espinosa Coarasa. Image Credits: Lai, et al. (2024). A robot-rodent interaction arena with adjustable spatial complexity for ethologically relevant behavioral studies. Cell Report
+</div>
+&nbsp
+
+Here’s what the Robot looks like. It’s a skid steer robot, that has two sets of wheels that drive a pair of treads.There are two PCB’s on the robot, one with an ESP32 microcontroller that controls the wheel speed commands, and another PCB   that control three LED’s that are used to track the location of the robot.
+
+<div style="text-align: center;" markdown="1">
+   <img src="images/robot_ctrl_diagram.png" alt="Breakdown of the Control of the Robot"/>
+   <figcaption> [How Does the Robot Move to a Destination?]
+</div>  
+&nbsp
+
+The robot receives instructions on where to move from a nearby server, which also holds the tracking information. These are converted into wheel commands that the robot can follow.
+
+## Virtual-Reality
+<div style="text-align: center;" markdown="1">
+   <img src="images/virtual_arena.png" alt="The Virtual Arena"/>
+   <figcaption> Virtual Habitat
+</div>
+&nbsp
+<div style="text-align: center;" markdown="1">
+   <img src="images/nav_mesh.png" alt="Navigation Mesh Area"/>
+   <figcaption> Depicts the area the real robot and mouse can move
+</div>
+&nbsp
+
+The Virtual Habitat is a 20-meter-long recreation of the Habitat. As with the original Habitat, you can add in Occlusions - hexagonal obstacles that occlude the view of the mouse. The Virtual Habitat is designed for a player to run in an open field to create a more immersive experience.
+
+Because the Virtual Habitat is designed for a larger environment, there needs to be a way to convert Virtual-Reality Coordinates to Canonical - the coordinates of the Habitat.
+
+Inside the game is a function to convert the Virtual-Reality Coordinates to Canonical and vice-versa. 
+
+The location of the VR Headset is always tracked - and as the player moves around the world, their coordinates are translated and sent to a server 
+
+In the event that the user spawns outside of the arena, there is a **soft-reset** button, that will **reset the orientation, position, yaw, and height** of the player. This is mapped to "X" on the Motion Controller. To reset the entire game, or **hard-reset the game**, the player can press "Y". 
+
+Occlusions are added into the Blueprint of the game itself. Simply specify which Occlusion you want to spawn by giving the location of the file containing the location indicies. 
+
+
+## Networking
+![Networking Diagram](images/networking_diagram.jpg)
+
+Networking is made possible using the TCP Messages Library written by Dr. Angel Germán Espinosa Coarasa. It allows for a **client-server** type of communication. In this case, the Virtual-Reality game is the client, and a server running on the Habitat PC receives all of the information from the client. The server broadcasts out messages, like the tracking information, out to its subscribed clients.
+
+I created a few small scripts in TCPTest, as a way of testing out the communication between the Virtual-Reality world and the server. TCPTest simulates the server itself, intercepting and interpreting messages. This helped confirm that the communication was working end to end. All of TCPTest was written using the TCP Messages library.
+
+As previously mentioned, TCP messages allows the the user to send messages, requests and responses and subscribe to a server. It allows for even more functionality by adding in custom functions. This is similar to the functions that exist in ROS nodes, which allow the user to define specific functionality.
+ 
+[In order to allow Port Forwarding Script to Work]
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+## Step-Up Module
+![Step-Up Module Diagram](images/step_up_diagram.png)
+
+**Overview:** The Step-Up Module is a *DC-to-DC converter that steps up 12V from the Virtual Reality (VR) backpack to 19.5V required by the VR Headset*. 
+Because there isn't a proprietary cable that connects the HP Reverb G2 VR backpack to the HP Reverb G2 Headset, I made my own!
+
+**Why?** Our Experiment requires a mobile agent moving in a large environment, which required us to extricate ourselves from a tethered power connection. This is because the Virtual Reality gear - the Headset and Backpack - requires a connection to an outlet..
+
+**How it Works:** As mentioned previously, this DC-to-DC converter takes 12V from the VR backpack steps it up to 19.5V, and goes into the headset. This allows the backpack and headset to be completely untethered to a wall outlet.   
+
+### Step-Up Module Bill of Materials
+Here is the Bill of Materials as well as pictures depicting how the step-up module is assembled. Not depicted are the spacers and screws necessary, as well as the DC-DC step-up converter itself. 
+
+Lastly is the fully assembled Step-Up Module
+
+[Step-Up Module BoM](https://docs.google.com/spreadsheets/d/1b9FdkY2aQWLmUtxk2oAGR2HAnpVTHtHQpYreBFGOhEA/edit?usp=sharing)
+
+#### Assembly of the Step-Up Module
+![Assembly of the Step-Up Module](images/assembly_1.jpg)
+
+#### Assembly with Numbered Parts
+![Assembly with numbered parts](images/assembly_2.jpg)
+
+#### Fully Assembled Step-Up Module
+![Fully Assembled Module](images/full_assembly.jpg)
+
+### Acknowledgements
+Angel Germán Espinosa Coarasa, Gabrielle Wink, Alex Lai, Chris Angeloni, Felix Alexander Maldonado, Selim Chalyshkan, Joe Reed,  Joshua Chi, and of course Dr. MacIver
+
+Further acknowledgement goes towards the MacIver Lab for several figures, which will be available in, Lai, et al. (2024) . A robot-rodent interaction arena with adjustable spatial complexity for ethologically relevant behavioral studies. Cell Reports
---
+++ main/python/Server.py
@@ -0,0 +1,206 @@
+import cellworld_experiment_service as ces
+import cellworld_tracking as ct
+import tcp_messages as tcp
+import cellworld as cw
+import sys
+import json
+import errno
+import os
+
+class Server:
+    def __init__(self) -> None:
+        print("=== BotEvade Server ===")
+        self.logs_folder = "../logs/"
+        self.tracking_service   = ServerTrackingService()
+        self.experiment_service = ServerExperimentService()
+        self.experiment_service.get_trajectory = self.tracking_service.get_trajectory
+        self.tracking_service.__process_step__ = self.experiment_service.__process_step__
+
+    def start(self)->bool:
+        if self.verify_logs_folder() is not True: 
+            self.create_logs_folder()
+
+        # check again to make sure 
+        if self.verify_logs_folder() is not True: 
+            self.log("Failed to create and find logs folder found.")
+
+        if not self.tracking_service.run():
+            self.log("ERROR! Failed to start Tracking Service.")
+            return False
+        
+        if not self.experiment_service.run():
+            self.log("ERROR! Failed to start Experiment Service.")
+            return False
+        return True
+    
+    def stop(self)->None:
+        self.experiment_service.stop()
+        self.tracking_service.stop()
+        self.exit()
+        return
+    
+    def exit(self)->None:
+        self.log("Exiting program")
+        sys.exit()
+
+    def create_logs_folder(self)->None:
+        self.log("Creating logs directory.")
+        os.mkdir(self.logs_folder)
+
+    def verify_logs_folder(self)->bool: 
+        if os.path.exists(self.logs_folder) and os.path.isdir(self.logs_folder):
+            self.log("Successfully found logs directory.")
+            return True
+        self.log("Failed to find logs directory.")
+        return False
+
+    def log(self, msg, *args):
+        additional_info = ', '.join(str(arg) for arg in args)
+        if additional_info:
+            print(f"[Server] {msg}. Info: {additional_info}")
+        else:
+            print(f"[Server] {msg}.")
+
+class ServerTrackingService(ct.TrackingService):
+    def __init__(self) -> None:
+        ct.TrackingService.__init__(self)
+        # super().__init__(self)
+        self.ip                  = "127.0.0.1"
+        self.on_new_connection   = self.on_connection_ts
+        self.router.add_route("send_step",self.send_step_ts,ces.Step)
+        self.current_trajectory = None
+        self.__process_step__   = None
+        
+    def run(self)->bool:
+        # print('here')
+        res = False
+        try:
+            res = self.start(port=4510)
+            self.log("Started service.",res)
+        except OSError:
+            self.log("lol server is still refreshing")
+        return res
+
+    def get_trajectory(self)->ces.Trajectories:
+        self.log("get trajectory")
+        trajectory = self.current_trajectory
+        self.current_trajectory = None
+        # self.log(trajectory)
+        return trajectory
+
+    def on_connection_ts(self,msg)->None:
+        self.log("New connection.",msg)
+    
+    def process_to_json_list(self,step:ces.Step = None)->None:
+        if step is None: 
+            return
+        self.trajectories
+        return None
+    
+    def process_step(self, step:ces.Step)->ces.Step: # not being called but i get a message back? 
+        # print(step)
+        if self.current_trajectory is None:
+            self.current_trajectory = ces.Trajectories()
+        if step is not None: self.current_trajectory.append(step)
+        else: self.log("step none")
+        
+        if self.current_trajectory is None: self.log("current trajectory is none")
+        self.__process_step__(step)
+        step = ces.Step()
+        step.agent_name = "predator"
+        return step
+
+    def send_step_ts(self,step:ces.Step = None):
+        step = self.process_step(step)
+        self.ts.broadcast_subscribed(ces.Message(step.agent_name + "_step", step))
+        return
+    
+    def log(self, msg, *args):
+        additional_info = ', '.join(str(arg) for arg in args)
+        if additional_info:
+            print(f"[TS] {msg}. Info: {additional_info}")
+        else:
+            print(f"[TS] {msg}.")
+    
+    def echo(self,msg):
+        self.log("message received:",msg)
+        
+class ServerExperimentService(ces.ExperimentService):
+    def __init__(self):
+        super().__init__()
+        self.world = cw.World.get_from_parameters_names("hexagonal", "canonical")
+        self.on_new_connection      = self.on_new_connection_es
+        self.on_experiment_started  = self.on_experiment_started_es
+        self.on_experiment_finished = self.on_experiment_finished_es
+        self.on_episode_started     = self.on_episode_started_es
+        self.on_episode_finished    = self.on_episode_finished_es
+        self.on_experiment_resumed  = self.on_experiment_resumed_es  
+        self.router.add_route("get_cells_locations", self.get_cells_locations)
+        self.router.add_route("get_occlusions", self.get_occlusions, str) 
+        # self.on_step                = self.on_step_ts 
+        self.set_tracking_service_ip("127.0.0.1")
+        self.current_trajectory = None
+        self.get_trajectory = None # tell TS to send us trajectory for this episode 
+        pass
+
+    def get_cells_locations(self, m=None) -> cw.Location_list:
+        return cw.Location_list([c.location for c in self.world.cells])
+    
+    def get_occlusions(self, occlusions_name) -> cw.Cell_group_builder:
+        self.log("received get_occlusions:", occlusions_name)
+        return cw.Cell_group_builder.get_from_name("hexagonal", occlusions_name, "occlusions")
+
+    def run(self)->bool:
+        res = self.start()
+        self.log(f"Started server", res)
+        self.join()
+        return res
+    
+    def _call_tracking_service_(self):
+        return
+
+    def save_trajectories_to_log(self,trajectories:ces.Trajectories=None)->bool:
+        if self.episode_in_progress:
+            return False
+        return True
+    
+    def on_new_connection_es(self,msg:tcp.Connection = None)->None:
+        self.log("New connection", msg.state)
+
+    def on_episode_started_es(self,msg:ces.StartEpisodeRequest = None)->None:
+        self.log("episode started",msg)
+      
+    def on_episode_finished_es(self,msg:bool = None)->None:
+        self.log("Episode finished.", msg)
+        if self.get_trajectory is not None: 
+            self.active_episode.trajectories = self.get_trajectory() 
+            self.log(self.active_episode)
+
+    def on_experiment_started_es(self,msg:ces.StartExperimentResponse = None)->None:
+        self.log("Experiment started.",f"experiment name: {msg.experiment_name}")
+    
+    def on_experiment_resumed_es(self,msg:ces.ResumeExperimentRequest = None)->None:
+        self.log("Experiment resumed",
+                 f"experiment name: {msg.experiment_name}", 
+                 f"duration extension: {msg.duration_extension}")
+
+    def on_experiment_finished_es(self,msg:bool = None)->None:
+        self.log("Experiment finished.",msg)
+  
+    def log(self, msg, *args):
+        additional_info = ', '.join(str(arg) for arg in args)
+        if additional_info:
+            print(f"[ES] {msg}. Info: {additional_info}")
+        else:
+            print(f"[ES] {msg}.")
+
+# as steps come in, append to traj JsonList and then save as trejectory into episode file. 
+# output: 1 trajlist, Experiment(Json):Episode_N:Trajectory
+# go to experiment.py -> line 28
+            
+
+def main():
+    s = Server()
+    s.start()
+if __name__ == "__main__":
+    main()
