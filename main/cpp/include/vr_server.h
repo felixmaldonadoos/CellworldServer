902851829863931d18b615b3d1ad32adc3eef213
felix <famaldonadoos@gmail.com>
2024-05-24 17:12:24 -0500

    cpp server up and running

--- main/cpp/include/vr_server.h
+++ main/cpp/include/vr_server.h
@@ -10,10 +10,20 @@ namespace vr_server {
     struct Vr_service : tcp_messages::Message_service {
         Routes(
             Add_route_with_response("start_experiment", start_experiment, experiment::Start_experiment_request);
+            Add_route_with_response("resume_experiment", resume_experiment, experiment::Resume_experiment_request);
+            Add_route_with_response("start_episode", start_episode, experiment::Start_episode_request);
+            Add_route_with_response("finish_episode", finish_episode);
+            Add_route_with_response("finish_experiment", finish_experiment, experiment::Finish_experiment_request);
+            Add_route_with_response("get_experiment", get_experiment, experiment::Get_experiment_request);
             Add_route_with_response("get_cell_locations", get_cell_locations);
             Add_route_with_response("get_occlusions", get_occlusions, std::string);
         )
-        experiment::Start_experiment_response start_experiment(experiment::Start_experiment_request);
+        experiment::Start_experiment_response start_experiment(experiment::Start_experiment_request &);
+        experiment::Resume_experiment_response resume_experiment(experiment::Resume_experiment_request &);
+        bool start_episode(const experiment::Start_episode_request &);
+        bool finish_episode();
+        bool finish_experiment(const experiment::Finish_experiment_request &);
+        experiment::Get_experiment_response get_experiment(const experiment::Get_experiment_request &);
         cell_world::Location_list get_cell_locations();
         cell_world::Cell_group_builder get_occlusions(std::string &);
     };
