b89e3584e0d3e77158abbef9304d350d1aafd6b9
felix <famaldonadoos@gmail.com>
2024-05-24 14:55:35 -0500

    updated server

--- main/cpp/include/vr_server.h
+++ main/cpp/include/vr_server.h
@@ -6,16 +6,14 @@
 #include <cell_world.h>
 
 // cannot be made static bc changes way we call the function
-
 namespace vr_server {
     struct Vr_service : tcp_messages::Message_service {
         Routes(
-        // inherited routes: experiment_service.h
-//            Add_route_with_response("start_experiment", start_experiment, Start_experiment_request);
+            Add_route_with_response("start_experiment", start_experiment, experiment::Start_experiment_request);
             Add_route_with_response("get_cell_locations", get_cell_locations);
             Add_route_with_response("get_occlusions", get_occlusions, std::string);
         )
-//        cell_world::
+        experiment::Start_experiment_response start_experiment(experiment::Start_experiment_request);
         cell_world::Location_list get_cell_locations();
         cell_world::Cell_group_builder get_occlusions(std::string &);
     };
@@ -25,7 +23,6 @@ namespace vr_server {
         bool prepare();
         void stop();
         void join();
-        bool start_experiment(const experiment::Start_experiment_request &);
         agent_tracking::Tracking_server tracking_server;
         experiment::Experiment_server experiment_server;
     };
