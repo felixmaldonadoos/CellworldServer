a4aa03328101ba4ffebc3fae5dc549331c1b2b97
felix <famaldonadoos@gmail.com>
2024-07-12 13:40:28 -0500

    update

--- main/cpp/git-dependencies/cellworld_agent_tracking/include/agent_tracking.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <agent_tracking/tracking_service.h>
-#include <agent_tracking/tracking_client.h>
--- main/cpp/git-dependencies/dependency_json-cpp/include/json_cpp/json_base64.h
+++
@@ -1,9 +0,0 @@
-#include <json_cpp/json_buffer.h>
-
-namespace json_cpp{
-    struct Json_base64{
-        static size_t size(const std::string &);
-        static void read(const std::string &, Json_buffer &);
-        static void write(std::ostream &,  const Json_buffer &);
-    };
-}
