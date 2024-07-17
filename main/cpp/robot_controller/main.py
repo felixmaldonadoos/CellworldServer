3d5de754d3e696d1b1dc57f8244bdbfa77d63cce
alex <famaldonadoos@gmail.com>
2024-07-17 09:13:30 -0700

    merged changes

--- main/cpp/robot_controller/main.py
+++ main/cpp/robot_controller/main.py
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-import cellworld as cw
-import cellworld_game as game
-import tcp_messages as tcp
-loader = game.CellWorldLoader(world_name="21_05")
-model = game.BotEvade(world_name="21_05",
-                      render=True,
-                      time_step=.025)
-model.prey.dynamics.turn_speed = 0
-model.prey.dynamics.forward_speed = 0
-model.reset()
-
-
-server = tcp.MessageServer()
-
-
-def move_mouse(message: tcp.Message):
-    step: cw.Step = message.get_body(body_type=cw.Step)
-    model.prey.state.location = (step.location.x, step.location.y)
-
-
-def get_predator_step(message):
-    predator_step = cw.Step(agent_name="predator")
-    predator_step.location = cw.Location(*model.predator.state.location)
-    return predator_step
-
-
-def reset(message):
-    model.reset()
-
-
-running = True
-
-
-def stop(message):
-    global running
-    running = False
-
-
-server.router.add_route("reset", reset)
-server.router.add_route("move_mouse", move_mouse)
-server.router.add_route("get_predator_step", move_mouse)
-server.router.add_route("stop", stop)
-
-server.allow_subscription = True
-server.start(666)
-
-# model.view.add_event_handler("mouse_move", move_mouse)
-while running:
-    model.step()
-    predator_step = cw.Step(agent_name="predator")
-    predator_step.location = cw.Location(*model.predator.state.location)
-    # predator_step.rotation = model.predator.state.direction
-    server.broadcast_subscribed(message=tcp.Message("predator_move", body=predator_step))
-
-
-=======
 import cellworld as cw
 import cellworld_game as game
 import tcp_messages as tcp
@@ -174,4 +117,3 @@ while running:
     predator_step.rotation = model.predator.state.direction
     server.broadcast_subscribed(message=tcp.Message("predator_step", body=predator_step))
     sample_count_predator += 1
->>>>>>> fixing
